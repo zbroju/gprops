fde6f95cb5a74ec65797e1aedc0d9341868e0033
Andrey Ivanov <anvivanov@gmail.com>
2016-06-30 23:00:43 -0400

    Split() replaced with SplitN()

--- gprops.go
+++ gprops.go
@@ -72,7 +72,7 @@ func (props *Properties) Load(r io.Reader) error {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if !strings.HasPrefix(line, commentPrefix) && len(line) > 0 {
-			keyValuePair := strings.Split(line, settingSeparator)
+			keyValuePair := strings.SplitN(line, settingSeparator, 2)
 			if len(keyValuePair) == 2 {
 				props.Set(strings.TrimSpace(keyValuePair[0]), strings.TrimSpace(keyValuePair[1]))
 			} else {
